78953a7a9dcf765f5f8ec0e268773cbb4979762f
lauramnss <silau0066@gmail.com>
2018-08-29 11:05:55 +0700

    hehe

---
+++ Biodata.js
@@ -0,0 +1,8 @@
+let myBiodata = {
+    Name : "Laura",
+    Class : "XIRPL",
+    Ages : 16,
+    School : "SMK Bina Mandiri Multimedia",
+    Hobbies: ["Reading", "Singing", "Badminton", "PlayMusic"]
+}
+     console.log("My Name is " + myBiodata.Name + "\nIam class " + myBiodata.Class + "\nAges " + myBiodata.Ages + "\nSchool " + myBiodata.School + "\nmyHobbies are " + myBiodata.Hobbies)
